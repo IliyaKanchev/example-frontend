12a7c0199f35db2b9a83c5b76c7caea6f1facdce
Iliya Kanchev <iliqtk@gmail.com>
2020-02-11 12:21:57 +0200

    - minor re-factor

--- gulpfile.js
+++ gulpfile.js
@@ -14,43 +14,47 @@ var paths = {
     dist: 'dist',
     distIndex: 'dist/index.html',
     distCSS: 'dist/**/*.css',
-    distJS: 'dist/**/*.js'
+    distJS: 'dist/**/*.js',
+
+    srcPathOptions: {
+        allowEmpty: true
+    }
   };
 
 gulp.task('sass', function(){
-    return gulp.src(paths.appSASS, { allowEmpty: true })
+    return gulp.src(paths.appSASS, paths.srcPathOptions)
         .pipe(sass()) // Converts Sass to CSS with gulp-sass
         .pipe(gulp.dest(paths.dist))
 });
 
 gulp.task('css', function () {
-    return gulp.src(paths.appCSS, { allowEmpty: true }).pipe(gulp.dest(paths.distCSS));
+    return gulp.src(paths.appCSS, paths.srcPathOptions).pipe(gulp.dest(paths.distCSS));
 });
 
 gulp.task('js', function () {
-    return gulp.src(paths.appJS, { allowEmpty: true }).pipe(gulp.dest(paths.distJS));
+    return gulp.src(paths.appJS, paths.srcPathOptions).pipe(gulp.dest(paths.distJS));
 });
 
 gulp.task('html', function () {
-    return gulp.src(paths.appHTML, { allowEmpty: true }).pipe(gulp.dest(paths.dist));
+    return gulp.src(paths.appHTML, paths.srcPathOptions).pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('copy', gulp.series('sass', 'css', 'js', 'html'));
 
 gulp.task('inject', gulp.series('copy', function(){
-    var css = gulp.src(paths.distCSS, { allowEmpty: true });
-    var js = gulp.src(paths.distJS, { allowEmpty: true });
+    var css = gulp.src(paths.distCSS, paths.srcPathOptions);
+    var js = gulp.src(paths.distJS, paths.srcPathOptions);
 
     var options = { relative: false, ignorePath: ["dist"] };
 
-    return gulp.src(paths.appIndex, { allowEmpty: true })
+    return gulp.src(paths.appIndex, paths.srcPathOptions)
         .pipe(inject( css, options))
         .pipe(inject( js, options))
         .pipe(gulp.dest(paths.dist));
 }));
 
 gulp.task('serve', gulp.series('inject', function(){
-    return gulp.src(paths.dist, { allowEmpty: true })
+    return gulp.src(paths.dist, paths.srcPathOptions)
         .pipe(webserver({
             port: 3000,
             livereload: true
